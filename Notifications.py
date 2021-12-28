e58cd53a480e8456105192efb8feaae85cfcc897
lukephilipps <85194228+lukephilipps@users.noreply.github.com>
2021-12-28 11:44:06 -0700

    Update Notifications.py

--- Notifications.py
+++ Notifications.py
@@ -3,17 +3,19 @@ import requests
 #https://discord.com/api/v9/channels/906478400921817102/messages URL for main channel
 
 header = {
-    'authorization': 'OTA2NDc3NDQ2MDA4ODE5NzEz.YYZNsw.6VjP8ne-jtdROB2gfZnz9xWRqiE'
+    'authorization': ''
 }
 
 def postMessageGeneral(classNum):
     payload = {
         'content': "@everyone " + str(classNum) + " seat currently available!"
     }
-    requests.post('https://discord.com/api/v9/channels/906478400921817102/messages', data=payload, headers=header)
+    requests.post('', data=payload, headers=header)
 
+# This method is completely optional, I set it up so that the bot posts a message when you first run the program.
+# Feel free to reimplement it if desired or ignore it.
 def BeginTesting():
     payload = {
         'content': "Beginning Testing..."
     }
-    requests.post('https://discord.com/api/v9/channels/906609313978675230/messages', data=payload, headers=header)
+    requests.post('', data=payload, headers=header)
