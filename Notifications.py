b6396edbd88c511f710ce546ad685c04f2cf9ce9
lukephilipps <85194228+lukephilipps@users.noreply.github.com>
2021-12-28 12:10:11 -0700

    Update Notifications.py

--- Notifications.py
+++ Notifications.py
@@ -4,6 +4,8 @@ header = {
     'authorization': '"""paste discord account authorization code here between single quotes"""'
 }
 
+# This method posts a message to a discord channel provided you put the channels url in the post method
+# The classNum parameter is so that the message can contain which class is open since you can check for multiple at once
 def postMessageGeneral(classNum):
     payload = {
         'content': "@everyone " + str(classNum) + " seat currently available!"
